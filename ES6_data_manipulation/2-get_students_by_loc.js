51c7f50a922fa32f078716b048250432558f11f6
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-07-22 11:16:14 +0200

    update

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,6 @@
 export default function getStudentsByLocation(students, city) {
-    return students.filter((student) => student.location === city);
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.filter((student) => student.location === city);
 }
