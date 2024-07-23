d5165b046e40325ae39165a8ea85a48bac43ba0d
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-07-23 09:52:52 +0200

    make task 8 and 9

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatos', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return groceries;
+}
