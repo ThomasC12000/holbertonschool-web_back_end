39dcc3be0b30afc9ac3d051a122a75be7343a154
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-07-23 08:57:47 +0200

    update task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-    return {
-        ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A',
-    };
+      const updatedGrade = newGrades.find((grade) => grade.studentId === student.id);
+      if (updatedGrade) {
+        return { ...student, grade: updatedGrade.grade };
+      }
+      return { ...student, grade: 'N/A' };
     });
 }
