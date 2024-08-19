8a6035e9f5797d3f7ce324444adfedc3f34b64d6
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-08-19 10:02:07 +0200

    update task 2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,16 +7,8 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
-    """measure_runtime"""
-    start_time = time.time()
-
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    total_time = time.time() - start_time
-    return total_time
+async def measure_runtime() -> float:
+    '''measure_runtime'''
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start
