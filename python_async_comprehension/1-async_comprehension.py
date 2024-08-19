10bf7c626dc1f1d48324d4ae5f66450c0ca8e62a
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-08-19 10:32:22 +0200

    update task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-'''Import async_generator from the previous task and then write
-a coroutine called async_comprehension that takes no arguments.'''
+"""Import async_generator from the previous task and then write
+a coroutine called async_comprehension that takes no arguments."""
 import asyncio
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
-    '''async comprehension'''
+    """async comprehension"""
     return [i async for i in async_generator()]
