d3e57bf47986480a17326d74c3bf7906e772554f
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-08-19 10:00:23 +0200

    update task 1 and 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Import async_generator from the previous task and then 
 write a coroutine called async_comprehension that takes no arguments."""
-
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension():
+    """async comprehension"""
     return [number async for number in async_generator()]
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,12 +2,13 @@
 """Import async_comprehension from the previous file and write 
 a measure_runtime coroutine that will execute 
 async_comprehension four times in parallel using asyncio.gather."""
-
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime():
+    """measure_runtime"""
     start_time = time.time()
 
     await asyncio.gather(
