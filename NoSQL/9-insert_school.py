fe57a64a17a781999273d14cbdf90ab7f5273158
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-08-21 14:12:58 +0200

    make task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a Python function that inserts a new document in a
+collection based on kwargs."""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert_school : Inserts a new document in a collection based on kwargs."""
+    if mongo_collection is None:
+        return None
+    return mongo_collection.insert_one(kwargs).inserted_id
