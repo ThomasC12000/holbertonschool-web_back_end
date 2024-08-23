de197c0fce0e4db2e1a63d8276601efcae431e87
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-08-23 14:17:41 +0200

    update

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Write a Python script that provides some stats
-about Nginx logs stored in MongoDB. """
+about Nginx logs stored in MongoDB."""
 
 from pymongo import MongoClient
 
