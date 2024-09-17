340c183e225379c8138eb84d6bd7fed19c9efeb4
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-09-17 09:58:55 +0200

    make task 0, 1, 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+function countStudents(chemin) {
+  let data;
+  try {
+    data = fs.readFileSync(chemin, "utf8");
+  } catch (err) {
+    throw new Error("Cannot load the database");
+  }
+
+  const lignes = data.trim().split("\n");
+  if (lignes.length <= 1) {
+    throw new Error("Cannot load the database");
+  }
+
+  const students = {};
+  let totalStudents = 0;
+
+  for (let i = 1; i < lignes.length; i += 1) {
+    const line = lignes[i].trim();
+    if (line) {
+      const [firstname, , , field] = line.split(",");
+      if (!students[field]) {
+        students[field] = [];
+      }
+      students[field].push(firstname);
+      totalStudents += 1;
+    }
+  }
+
+  console.log(`Number of students: ${totalStudents}`);
+  for (const field in students) {
+    if (Object.prototype.hasOwnProperty.call(students, field)) {
+      console.log(
+        `Number of students in ${field}: ${
+          students[field].length
+        }. List: ${students[field].join(", ")}`
+      );
+    }
+  }
+}
+
+module.exports = countStudents;
